db8733631cf80f0e410de139fb5975f35251b660
SneyderHOL <1507@holbertonschool.com>
2020-04-28 14:00:48 -0500

    [FIX]: typo error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+last_digit = number % 10
 if number < 0:
     last_digit = (number * -1) % 10
     last_digit = last_digit * -1
-else:
-    last_digit = number % 10
 if last_digit > 5:
-    print('Last digit of ' + number.__str__() + ' is ' + last_digit.__str__() + ' and is greater that 5')
+    print('Last digit of ' + number.__str__() + ' is ' + last_digit.__str__() + ' and is greater than 5')
 elif last_digit == 0:
     print('Last digit of ' + number.__str__() + ' is ' + last_digit.__str__() + ' and is 0')
 else:
