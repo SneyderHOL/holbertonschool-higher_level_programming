f7163f999e6266dfb53d4fee76d99c9f9d11b35e
SneyderHOL <1507@holbertonschool.com>
2020-04-30 17:06:58 -0500

    Add task 9

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import fast_alphabet
+print(fast_alphabet.print_alphabet())
---
+++ 0x02-python-import_modules/fast_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_alphabet():
+    result = ''
+    for i in range(65, 91):
+        result += chr(i)
+    return result
