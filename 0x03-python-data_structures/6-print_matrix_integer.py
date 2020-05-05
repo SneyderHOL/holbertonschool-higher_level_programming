b3631187c535f0a0add0237bdffaf035f50b099b
SneyderHOL <1507@holbertonschool.com>
2020-05-05 16:20:18 -0500

    Add task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for mat in matrix:
+        for i in range(len(mat)):
+            if i == len(mat) - 1:
+                print("{}".format(mat[i]), end="")
+            else:
+                print("{}".format(mat[i]), end=" ")
+        else:
+            print("")
