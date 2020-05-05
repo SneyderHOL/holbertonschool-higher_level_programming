ae3cc40417bef6256c3128edb90b36f43f57927f
SneyderHOL <1507@holbertonschool.com>
2020-05-05 17:46:57 -0500

    [FIX]: remove return None

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if (len(my_list) == 0):
-        return None
-    num = 0
-    for i in range(len(my_list)):
-        tmp = my_list[i]
-        if (num < tmp):
-            num = tmp
-    return num
+    if my_list:
+        num = 0
+        for i in range(len(my_list)):
+            tmp = my_list[i]
+            if (num < tmp):
+                num = tmp
+        return num
