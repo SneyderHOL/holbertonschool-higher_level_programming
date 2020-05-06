1e73bb8a92eae6d733cf9a4da61f4bf8ccf0930c
SneyderHOL <1507@holbertonschool.com>
2020-05-05 23:16:14 -0500

    [FIX]: add reverse list function

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,35 +10,55 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *start = NULL, *end = NULL, *tmp = NULL;
-	int count = 0, aux = 0;
+	listint_t *aux = NULL, *middle = NULL;
+	int count = 0;
 
 	if (head == NULL || *head == NULL)
 		return (1);
-	start = *head, end = *head;
-	for (count = 1; end->next != NULL; count++)
-		end = end->next;
-	if (*head == end)
-		return (1);
-	for (aux = 1; aux <= count / 2; aux++)
+	aux = *head, middle = *head;
+	for (count = 1; aux->next != NULL; count++)
 	{
-		if (count % 2 == 0 && aux == count / 2)
+		middle = middle->next;
+		aux = aux->next;
+		if (aux->next != NULL)
+			aux = aux->next, count++;
+	}
+	printf("%d\n", count);
+	if (*head == aux)
+		return (1);
+	aux = *head;
+	middle = reverse_listint(&middle);
+	while (middle->next != NULL)
+	{
+		if (aux->n == middle->n)
 		{
-			if (start->n == end->n)
-				return (1);
-			else
-				return (0);
-		}
-		if (start->n == end->n)
-		{
-			start = start->next;
-			tmp = start;
-			while (tmp->next != end)
-				tmp = tmp->next;
-			end = tmp;
+			aux = aux->next;
+			middle = middle->next;
 		}
 		else
 			return (0);
 	}
 	return (1);
+}
+/**
+ * reverse_listint - function that reverse a listint_t linked list.
+ * @head: pointer to the head of a listint element
+ * Return: a pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *aux = *head;
+	listint_t *tmp = *head;
+
+	if (*head == NULL)
+		return (NULL);
+	while (aux->next != NULL)
+	{
+		if (tmp != *head)
+			tmp = *head;
+		*head = aux->next;
+		aux->next = (*head)->next;
+		(*head)->next = tmp;
+	}
+	return (*head);
 }
