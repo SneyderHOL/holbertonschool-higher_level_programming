93bbdfa94368bcd742db76a1f6bab568308db47d
SneyderHOL <1507@holbertonschool.com>
2020-05-07 15:42:25 -0500

    str validation2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     dic = roman_dictionary()
     word = roman_string
     num = 0
-    if word is str and word is not None:
+    if word is str and word is not None and word.isalpha():
         for i in range(len(word)):
             if exist_in_dictionary(dic, word[i]):
                 if i != len(word) - 1:
