111a0bfaa979b36455228e932f82b5fb5e2c2f18
SneyderHOL <1507@holbertonschool.com>
2020-05-07 12:45:15 -0500

    [FIX]: typo error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     if a_dictionary:
         sort_keys = sorted(a_dictionary)
         for k in sort_keys:
-            print("{} : {}".format(k, a_dictionary[k]))
+            print("{}: {}".format(k, a_dictionary[k]))
