43b2bea5740ce4e087b5369781c3cd6c99c4d157
SneyderHOL <1507@holbertonschool.com>
2020-05-07 15:34:54 -0500

    [FIX]: change iterator in string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dictionary = roman_dictionary()
-    num, sub = 0, 0
-    if roman_string.isalpha():
-        for c in roman_string:
-            if c in dictionary:
-                if ((sub == 1 and dictionary[c] == 10) or
-                        (sub == 1 and dictionary[c] == 5)):
-                    num += dictionary[c] - 2
+    dic = roman_dictionary()
+    word = roman_string
+    num = 0
+    if word.isalpha():
+        for i in range(len(word)):
+            if exist_in_dictionary(dic, word[i]):
+                if i != len(word) - 1:
+                    if dic[word[i]] >= dic[word[i + 1]]:
+                        num += dic[word[i]]
+                    else:
+                        num -= dic[word[i]]
                 else:
-                    num += dictionary[c]
-                sub = dictionary[c]
+                    num += dic[word[i]]
             else:
                 num = 0
                 break
@@ -21,3 +23,7 @@ def roman_dictionary():
     dictionary = ({'I': 1, 'V': 5, 'X': 10, 'L': 50,
                    'C': 100, 'D': 500, 'M': 1000})
     return dictionary
+
+
+def exist_in_dictionary(dictionary, value):
+    return value in dictionary
