a8955bf10705461de72eb9dddbe44bf12fd0c497
SneyderHOL <1507@holbertonschool.com>
2020-05-18 12:22:58 -0500

    [FIX]: print new line

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,8 +7,6 @@ def safe_print_list(my_list=[], x=0):
                 print(my_list[i], end='')
                 ret += 1
             except (IndexError, ValueError):
-                print('')
                 break
-        else:
-            print('')
+    print('')
     return ret
