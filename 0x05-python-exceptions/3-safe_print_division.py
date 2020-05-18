52d60ef6d3adfc0005943726f4d6a3f88273398c
SneyderHOL <1507@holbertonschool.com>
2020-05-18 14:19:48 -0500

    [FIX]: print with format

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError):
         ret = None
     finally:
-        print('Inside result:', ret)
-        return ret
+        print('Inside result: {}'.format(ret))
+    return ret
