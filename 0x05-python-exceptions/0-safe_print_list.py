6fcec32e9464107807410500e76c1cd23c42b3c7
SneyderHOL <1507@holbertonschool.com>
2020-05-18 12:20:14 -0500

    [FIX]: exception type

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     ret = 0
     if my_list:
-        if (x > my_list.__len__()):
-            x = my_list.__len__()
         for i in range(x):
             try:
-                print(my_list[i] * 1, end='')
-            except TypeError:
-                print('Element not an integer')
+                print(my_list[i], end='')
+                ret += 1
+            except (IndexError, ValueError):
+                print('')
+                break
         else:
             print('')
-        ret = i + 1
     return ret
