31c2d3838c8a95a33394a6fb98fd307f67969085
SneyderHOL <1507@holbertonschool.com>
2020-05-19 17:41:11 -0500

    [FIX]: my_print function conditions

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,6 +19,16 @@ class Square:
 
     """
     def __init__(self, size=0, position=(0, 0)):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        elif (
+                type(value) != tuple or len(value) != 2 or
+                type(value[0]) != int or type(value[1]) != int or
+                value[0] < 0 or value[1] < 0
+        ):
+            raise TypeError('value must be a tuple of 2 positive integers')
         self.__size = size
         self.__position = position
 
@@ -79,12 +89,14 @@ class Square:
     def my_print(self):
         if self.__size > 0:
             if self.__position[1] > 0:
-                for i in range(self.__size):
-                    for j in range(self.__position[0]):
-                        print(' ', end="")
-                    for k in range(self.__size):
-                            print('#', end="")
+                for x in range(self.__position[1]):
                     print('')
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(' ', end="")
+                for k in range(self.__size):
+                    print('#', end="")
+                print('')
         else:
             print('')
 
