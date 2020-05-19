ee6352f18e3514a9c48321d3435bd3b6fb04bb73
SneyderHOL <1507@holbertonschool.com>
2020-05-19 15:19:08 -0500

    [FIX]: add documentation (docstrings)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 class Square:
+    """__init__: method to initilize object's properties.
+
+    Args:
+        size (int): The size of the square object to initialize.
+
+    Attributes:
+        __size (int): The size of the square object.
+
+    """
     def __init__(self, size):
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 class Square:
+    """__init__: method to initilize object's properties.
+
+    Args:
+        size (int): The size of the square object to initialize.
+
+    Attributes:
+        __size (int): The size of the square object.
+
+    Raises:
+        TypeError: if size is not an integer
+        ValueError: If size is less than 0
+
+    """
     def __init__(self, size=0):
         try:
             if size >= 0:
