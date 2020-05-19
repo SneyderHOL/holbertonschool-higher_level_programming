02dfca52cc84fe37f92c45182120484fb11b4ca2
SneyderHOL <1507@holbertonschool.com>
2020-05-19 15:56:03 -0500

    [FIX]: type condition

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,10 +17,8 @@ class Square:
 
     """
     def __init__(self, size=0):
-        try:
-            if size >= 0:
-                self.__size = size
-            else:
-                raise ValueError('size must be >= 0')
-        except TypeError:
+        if type(size) != int:
             raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,13 +17,11 @@ class Square:
 
     """
     def __init__(self, size=0):
-        try:
-            if size >= 0:
-                self.__size = size
-            else:
-                raise ValueError('size must be >= 0')
-        except TypeError:
+        if type(size) != int:
             raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
 
     """area: method to calculate the area of the square object
 
