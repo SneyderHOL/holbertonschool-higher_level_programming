64406cabb9a0be0e6502c5dcaef8ba3f65447d05
SneyderHOL <1507@holbertonschool.com>
2020-05-19 20:26:37 -0500

    [FIX]: comments location

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,41 +1,47 @@
 #!/usr/bin/python3
-import math
+
 """  MagicClass Class
     Attributes:
         _MagicClass__radius (int): The radius of a circle.
 """
+import math
 
 
 class MagicClass:
-    """__init__: method to initilize object's properties.
-
-    Args:
-        radius (int or float): The radius of the object to initialize.
-
-    Raises:
-        TypeError: if radius is not an integer or if is not a float
-
+    """  MagicClass Class
+        Attributes:
+            _MagicClass__radius (int): The radius of a circle.
     """
+
     def __init__(self, radius=0):
+        """__init__: method to initilize object's properties.
+
+        Args:
+            radius (int or float): The radius of the object to initialize.
+
+        Raises:
+            TypeError: if radius is not an integer or if is not a float
+
+        """
         self._MagicClass__radius = 0
         if (type(radius) is not int) and (type(radius) is not float):
             raise TypeError("radius must be a number")
         self._MagicClass__radius = radius
 
-    """area: method to calculate the area of a circle object
-
-    Return:
-        The return value: The object radius to the power 2 times pi
-
-    """
     def area(self):
+        """area: method to calculate the area of a circle object
+
+        Return:
+            The return value: The object radius to the power 2 times pi
+
+        """
         return self._MagicClass__radius ** 2 * math.pi
 
-    """circumference: method to calculate the circumference of a circle object
-
-    Return:
-        The return value: The object radius times 2 times pi
-
-    """
     def circumference(self):
+        """circumference: calculate the circumference of a circle object
+
+        Return:
+            The return value: The object radius times 2 times pi
+
+        """
         return 2 * math.pi * self._MagicClass__radius
