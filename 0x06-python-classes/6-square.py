c3cecd8b743f54856b26bb47c3c34be18c521418
SneyderHOL <1507@holbertonschool.com>
2020-05-20 12:12:29 -0500

    [FIX]: use of setters in init method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,18 +19,8 @@ class Square:
 
     """
     def __init__(self, size=0, position=(0, 0)):
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        elif (
-                type(position) != tuple or len(position) != 2 or
-                type(position[0]) != int or type(position[1]) != int or
-                (position[0] < 0 or position[1] < 0)
-        ):
-            raise TypeError('position must be a tuple of 2 positive integers')
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     """size: method to get the size attribute
 
