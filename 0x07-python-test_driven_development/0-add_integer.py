6acc09249068ee318fc0c8ad58790c2c434db2d3
SneyderHOL <1507@holbertonschool.com>
2020-05-23 09:09:04 -0500

    [FIX]: cast to resul

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,8 +12,14 @@ def add_integer(a, b=98):
     Raises:
         TypeError: if a or b are not an integer
     """
-    if type(a) != int and type(a) != float:
-        raise TypeError('a must be an integer')
-    if type(b) != int and type(b) != float:
-        raise TypeError('b must be an integer')
-    return int(a) + int(b)
+    if type(a) != int:
+        if type(a) == float:
+            a = int(a)
+        else:
+            raise TypeError('a must be an integer')
+    if type(b) != int:
+        if type(b) == float:
+            b = int(b)
+        else:
+            raise TypeError('b must be an integer')
+    return int(a + b)
