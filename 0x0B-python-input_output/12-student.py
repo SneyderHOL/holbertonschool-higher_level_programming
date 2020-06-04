6964f3f1be3d4e8fa1f7a7b845f4487b86ef8734
SneyderHOL <1507@holbertonschool.com>
2020-06-03 21:07:02 -0500

    Add task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""  Module for Student Class  """
+
+
+class Student:
+    """  Student class """
+    def __init__(self, first_name, last_name, age):
+        """__init__: function to initialize the student object
+        Args:
+            first_name (string): first name attribute for the student object
+            last_name (string): last name attribute for the student object
+            age (int): name attribute for the student object
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to_json: method that retrieves a dictionary representation of a
+            Student instance
+        Args:
+            attrs (list): list of strings
+        """
+        if attrs:
+            ret_dict = {}
+            for a in attrs:
+                if a in self.__dict__:
+                    ret_dict[a] = self.__dict__.get(a)
+            return ret_dict
+        else:
+            return self.__dict__
