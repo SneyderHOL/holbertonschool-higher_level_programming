df83445196a806c029e48fe0e8c87d29a1858898
SneyderHOL <1507@holbertonschool.com>
2020-06-03 22:02:03 -0500

    [FIX]: handle exception

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,16 +3,20 @@
 import sys
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-if __name__ == '__main__':
-    filename = 'add_item.json'
-    n_list = []
-    size = len(sys.argv) - 1
-    if size > 0:
+
+filename = 'add_item.json'
+n_list = []
+size = len(sys.argv) - 1
+if size > 0:
+    try:
         n_list = load_from_json_file(filename)
+    except Exception:
+        n_list = []
+    finally:
         i = 0
         while i < size:
             n_list.append(sys.argv[i + 1])
             i += 1
         save_to_json_file(n_list, filename)
-    else:
-        save_to_json_file(n_list, filename)
+else:
+    save_to_json_file(n_list, filename)
