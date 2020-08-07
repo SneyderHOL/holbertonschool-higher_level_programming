6ae02aeb36d2adf97809f43efa0334e9ab00de27
SneyderHOL <1507@holbertonschool.com>
2020-08-07 22:09:28 +0000

    Add task 2 to 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 """Select states module"""
+
+
+def escape_chars(string):
+    """ function to escape characers use in sql"""
+    esc_array = ['\\x00', '\\n', '\\r', '\\', '\'', '"', '\\x1a', '%', '_']
+    if type(string) is str:
+        for element in esc_array:
+            if element in string:
+                string = string.replace(element, "\\" + element)
+    return string
+
 if __name__ == "__main__":
     import MySQLdb
     import sys
@@ -9,8 +20,9 @@ if __name__ == "__main__":
         db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
-        query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id"\
-                " ASC".format(sys.argv[4])
+        var_input = escape_chars(sys.argv[4])
+        query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id "\
+                "ASC".format(var_input)
         res = cur.execute(query)
         results = cur.fetchall()
         for result in results:
