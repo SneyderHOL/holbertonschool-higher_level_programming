c7f6871b397db4c414396615c7d9da8fb5278a94
SneyderHOL <1507@holbertonschool.com>
2020-08-08 05:30:35 +0000

    [UPDATE]: remove port parameter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     size = len(sys.argv) - 1
     db = None
     if size == 3:
-        db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+        db = MySQLdb.connect(host='localhost', user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
         query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
