32ffc9472aabdcb98966e235960217d44c3a0b97
SneyderHOL <1507@holbertonschool.com>
2020-08-09 19:46:17 +0000

    [UPDATE]: formating output result

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -27,6 +27,6 @@ if __name__ == "__main__":
                 if state != s:
                     state = s
                     print("{}: {}".format(s.id, s.name))
-                print("\t{}: {}".format(c.id, c.name))
+                print("    {}: {}".format(c.id, c.name))
         session.commit()
         session.close()
