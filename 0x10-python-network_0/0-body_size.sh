e0886170d880c0eb6c4b6d1832632b0d8b6935c8
SneyderHOL <1507@holbertonschool.com>
2020-08-18 21:13:54 +0000

    [UPDATE]: 3 line scripts

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-#Bash script that takes in a URL, sends a request to that URL and display size
-#of the body of the response
+#Bash script that takes in a URL, sends a request and display size of the body
 curl -sI "$1" | grep Content-Length: | cut -d ':' -f 2 | tr -d ' '
