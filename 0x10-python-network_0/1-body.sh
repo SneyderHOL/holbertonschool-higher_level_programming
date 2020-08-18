d9ab5e9e7b007870160c886312e9c2a620d2b312
SneyderHOL <1507@holbertonschool.com>
2020-08-18 21:22:35 +0000

    [UPDATE]: shebang line modification

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 #Bash script that takes in a URL, sends a GET request and displays the body
-result=$(curl -sI "$1" -X GET -w http-out)
-status=$(echo "$result" | grep HTTP/ | cut -d " " -f 2)
-if [ "$status" == "200" ]; then
-    curl "$1"
-fi
+if $(curl -s -o /dev/null -w "%{http_code}" "$1") == "200"; then curl -s "$1"; fi
