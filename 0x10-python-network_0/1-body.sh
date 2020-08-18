226d72bd2375d0a9a5d45dab5e53935703cd8a39
SneyderHOL <1507@holbertonschool.com>
2020-08-18 21:33:26 +0000

    [UPDATE]: shellcheck complient

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a GET request and displays the body
-if $(curl -s -o /dev/null -w "%{http_code}" "$1") == "200"; then curl -s "$1"; fi
+if [ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" == "200" ]; then curl -s "$1"; fi
