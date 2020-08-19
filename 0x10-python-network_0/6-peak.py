64beda75fca9528b73ba199a85bcc4ed53ada4d9
SneyderHOL <1507@holbertonschool.com>
2020-08-19 03:10:18 +0000

    [UPDATE]: use of max

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,15 +5,4 @@
 def find_peak(list_of_integers):
     """Find a peak function"""
     if len(list_of_integers) > 0:
-        if len(list_of_integers) == 1:
-            return list_of_integers[0]
-        else:
-            mid = len(list_of_integers) // 2
-            peak = list_of_integers[mid]
-            left = find_peak(list_of_integers[:mid])
-            right = find_peak(list_of_integers[mid:])
-            if left > peak:
-                peak = left
-            if right > peak:
-                peak = right
-            return peak
+        return max(list_of_integers)
