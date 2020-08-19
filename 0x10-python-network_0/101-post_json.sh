22d2e91cfadd189e3b9c6966f0c17cf90407f029
SneyderHOL <1507@holbertonschool.com>
2020-08-19 01:45:40 +0000

    [UPDATE]: add s option

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that sends a JSON POST to a URL
-curl -X POST -H "Content-Type: application/json" -d "@$2" "$1"
+curl -sL -X POST -H "Content-Type: application/json" -d "@$2" "$1"
