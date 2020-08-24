34eb22829b32c5d357c04c35a16fe7fe0867ec3b
SneyderHOL <1507@holbertonschool.com>
2020-08-24 19:31:11 +0000

    [UPDATE]: add missin req variable

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """Fetch to a page module """
 import urllib.request
-from urllib.error import HTTPError
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    req = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(req) as response:
             page = response.read().decode("utf-8")
             print(page)
-    except HTTPError as e:
+    except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
