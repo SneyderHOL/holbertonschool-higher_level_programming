7e6d29e133e3c4a25217a66740f5a80cbe2a47f3
SneyderHOL <1507@holbertonschool.com>
2020-09-16 02:03:08 +0000

    [UPDATE]: variable scope

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@ const myNum1 = parseInt(process.argv[2]);
 const myNum2 = parseInt(process.argv[3]);
 add(myNum1, myNum2);
 function add (a, b) {
-  if (isNaN(myNum1) || isNaN(myNum2)) {
+  if (isNaN(a) || isNaN(a)) {
     console.log(NaN);
   } else {
     console.log('' + (a + b));
