bad1512099fd69193edfec150d7f1a152e0c0e4d
SneyderHOL <1507@holbertonschool.com>
2020-09-22 20:34:06 +0000

    [UPDATE]: manage callback function in readFile function

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const file = process.argv[2];
 const fs = require('fs');
-try {
-  const content = fs.readFileSync(file, 'utf8');
-  console.log(content);
-} catch (err) {
-  console.log(err);
-}
+fs.readFile(file, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
