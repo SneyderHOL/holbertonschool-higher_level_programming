be3ab47bf3f9a309d16fe129d259b32c9baee1c3
SneyderHOL <1507@holbertonschool.com>
2020-09-22 19:30:00 +0000

    Add README.md file

--- 0x14-javascript-web-scraping/0-readme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const file = process.argv[2];
-const fs = require('fs');
-try {
-  const content = fs.readFileSync(file, 'utf8');
-  console.log(content);
-} catch (err) {
-  console.log(err);
-}
