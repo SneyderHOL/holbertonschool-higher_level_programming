10278dbd919a938ccf128386612150788aaa5d2c
SneyderHOL <1507@holbertonschool.com>
2020-09-22 20:44:10 +0000

    Add task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const file = process.argv[2];
+const content = process.argv[3];
+const fs = require('fs');
+fs.writeFile(file, content, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(content);
+  }
+});
